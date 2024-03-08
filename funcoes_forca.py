23b485ca3e5109826a1659bea2b4fcd100dc214a
moises <moiseszandonai1234@gmail.com>
2024-03-07 21:30:29 -0300

    Update funcoes_forca.py

--- funcoes_forca.py
+++ funcoes_forca.py
@@ -1,4 +1,7 @@
 from random import choice
+errors = 0
+
+found_word = False
 
 def draw_word() -> str:
     sort_list = ['moises', 'cajado', 'mar', 'porta', 'monitor']
@@ -18,14 +21,37 @@ def validate_guess(guess:str)->bool:
         return False
     
     return True
+
+def check_guess(guess:str):
+   
+    if len(guess)  == 1:
+        if guess in selected_word:
+            for i, letter in enumerate(selected_word):
+                if guess  == letter:
+                    guess_word[i] = letter
+        else:
+            show_error()
+        found_word = "_" not in guess_word
+    elif guess == selected_word:
+        found_word = True
+    else:
+        if guess == selected_word:  
+            found_word = True
+        else:
+            show_error()
+
+   
+
+def show_error():
+    errors += 1
+    print("Palpite incorreto")
+
 selected_word = draw_word()
 
 
 guess_word = ['_' for _ in selected_word]
 
-errors = 0
 
-found_word = False
 
 while errors != 3 and not found_word:
     print(' '.join(guess_word))
@@ -33,5 +59,5 @@ while errors != 3 and not found_word:
 
     if not validate_guess(guess):
         continue
-    
-    
+    check_guess(guess)
+
